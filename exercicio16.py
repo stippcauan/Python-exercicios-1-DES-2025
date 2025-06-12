12bc83c01caae4478433c6fd5a93e504f005c646
stippcauan <cauan.stipp@escola.pr.gov.br>
2025-06-12 14:52:33 +0000

    so falta a 19

---
+++ exercicio16.py
@@ -0,0 +1,14 @@
+#Crie um programa que calcule o reajuste de salário:
+
+#Salários até R$ 2000,00: +15%
+#De R$ 2000,01 a R$ 5000,00: +10%
+#Acima de R$ 5000,00: +5%
+
+salario = float(input("Digite seu salario."))
+
+if salario <= 2000:
+    print("Você teve 15% a mais.")
+elif salario >= 2000.01 <= 5000.01:
+    print("Você teve 10% a mais")
+else:
+    print("Você teve 5% a mais.")
---
+++ exercicio20.py
@@ -0,0 +1,10 @@
+#Simule um login com nome de usuário "admin" e senha "1234".
+#Caso os dados estejam corretos, exiba "Acesso concedido", senão "Acesso negado".
+
+usuario = input("Digite o usuario.")
+senha = input("Digite a senha.")
+
+if usuario == ("cauan") and senha ("2010"):
+    print("Acesso concedido") 
+else:
+    print("Acesso negado")
